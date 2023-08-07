82b259c1619a0051e3dd2c428d56a58b62e5053a
arman1919 <arman192001@mail.ru>
2023-08-07 14:22:47 +0400

    update binary search, added recursive version

--- Homework_1/Binary_Search.py
+++ Homework_1/Binary_Search.py
@@ -1,3 +1,4 @@
+# binary_search (recursive) in Python
 
 
 def binary_search(arr:list,target:int) -> int:
@@ -21,14 +22,45 @@ def binary_search(arr:list,target:int) -> int:
     
     return "Target not found"
 
+ 
 
+def binary_search_recursive(arr:list,target:int,left,right):
+    
+    mid = (left+right) // 2
+    
+   
+    
+    if left > right:
+        
+        return "Target not found"
+    
+    
+    elif arr[mid] == target:
+        return mid
+    
+    elif target < arr[mid]:
+        return binary_search_recursive(arr,target,0,mid-1)
+           
+    else:
+        return binary_search_recursive(arr,target,mid+1,len(arr)-1)
 
+        
+            
+    
 
 
 ls = [1,2,3,4,5,6,7,8,9]
 
+print(binary_search(ls,1))
 print(binary_search(ls,5))
+print(binary_search(ls,9))
 print(binary_search(ls,12))
+print()
+print(binary_search_recursive(ls,1,0,len(ls)-1))
+print(binary_search_recursive(ls,6,0,len(ls)-1))
+print(binary_search_recursive(ls,9,0,len(ls)-1))
+print(binary_search_recursive(ls,12,0,len(ls)-1))
+
 
 
 
