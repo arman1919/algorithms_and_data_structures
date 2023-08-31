91c46cbbf4e967fa4303e4f24311c9e1353b3832
arman1919 <arman192001@mail.ru>
2023-08-31 12:30:25 +0400

    homework 6

--- Homework_6/hash_table_int-str.py
+++ Homework_6/hash_table_int-str.py
@@ -8,13 +8,27 @@ class MyHashTable:
         self.el_count = 0
     
     
-    def hash_integer(self,n):
-        A = (5 ** 0.5 - 1) / 2 
-        result = n * A
-        result -= int(result)  
-        result *= self.size
-        return int(result)
- 
+    def _hash(self,n):
+        
+        if isinstance(n,int):
+            A = (5 ** 0.5 - 1) / 2 
+            result = n * A
+            result -= int(result)  
+            result *= self.size
+            return int(result)
+        
+        elif isinstance(n,str):
+            hash_value = 0
+            p = 31  
+            
+            for char in n:
+                hash_value = (hash_value * p + ord(char)) % self.size
+
+            return hash_value
+        
+        else:
+            return KeyError
+        
       
     def check_LF(self):
         lf = self.el_count / self.size
@@ -26,7 +40,7 @@ class MyHashTable:
     
     def put(self,key,value):
         self.el_count += 1
-        index = self.hash_integer(key)
+        index = self._hash(key)
         linked_list = self.table[index]
         node = linked_list.return_to_key(key)
         if node != None:
@@ -42,7 +56,8 @@ class MyHashTable:
         
         
     def get(self,key):
-        index = self.hash_integer(key)
+        index = self._hash(key)
+        
         linked_list = self.table[index]
         if linked_list.empty():
             raise KeyError
@@ -57,7 +72,7 @@ class MyHashTable:
     
     
     def remove(self, key):
-        index = self.hash_integer(key)
+        index = self._hash(key)
         linked_list = self.table[index]
         cuurent = linked_list._head
         prev = None
@@ -82,25 +97,21 @@ ht.put(1,15)
 ht.put(2,16)
 ht.put(3,17)
 ht.put(4,18)
-ht.put(5,15)
+ht.put("str1",15)
+ht.put("str2",145)
+ht.put("str3",156)
 ht.put(6,16)
 ht.put(7,17)
 ht.put(8,18)
+print(ht.get("str2"))
 
-for i in ht.table:
-    print(i)
-    
-print()
-
-ht.put(9,18)
-
-ht.remove(9)
 
 for i in ht.table:
     print(i)
 
 
 print(ht.size)
+
     
 
 
